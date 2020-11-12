edf86cce85cf09d43ffb8697ebdd3fd750eed176
Telles01 <bernardtelgon@gmail.com>
2020-11-12 17:31:14 -0300

    changes

---
+++ snake.js
@@ -0,0 +1,13 @@
+const cvs = document.getElementById("snake");
+const ctx = document.getElementById("2d");
+
+// creste the unit 
+
+const box = 32;
+
+// load imagens
+
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const foodImg = new Image();
