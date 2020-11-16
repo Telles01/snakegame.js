d4917b55f1582282f35bb19d8a1d9004a8ea5570
Telles01 <bernardtelgon@gmail.com>
2020-11-16 13:56:42 -0300

    builting snake

--- draw.js
+++ draw.js
@@ -8,12 +8,21 @@ var snake;
 
 (function setup(){
     snake = new Snake();
+    fruit = new Fruit();
+    fruit.pickLocation();
+    
     
 
     window.setInterval(() => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        fruit.draw();
         snake.update();
         snake.draw();
+
+        if (snake.eat(fruit)) {
+            fruit.pickLocation();
+        }
+        
     }, 250);
 }());
 
--- fruit.js
+++ fruit.js
@@ -7,4 +7,9 @@ function Fruit() {
         this.x =(Math.floor(Math.random()* rows -1) + 1) * scale;
         this.y =(Math.floor(Math.random()* columns -1) + 1) * scale;
     }
+
+    this.draw = function() {
+        ctx.fillStyle = "#E40A0AF";
+        ctx.fillRect(this.x, this.y, scale, scale)
+    }
 }
